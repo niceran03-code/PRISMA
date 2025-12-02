35f335a9b619fef1491881be192a7ee4d2c3b56b
ran <niceran03@gmail.com>
2025-12-02 16:04:32 +0800

    Building a REST API with NestJS and Prisma:Handling Relation Data

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -17,8 +17,17 @@ export class ArticlesService {
     return this.prisma.article.findMany({ where: { published: true } });
   }
 
-  findOne(id: number) {
-    return this.prisma.article.findUnique({ where: { id } });
+  async findOne(id: number) {
+    const article = await this.prisma.article.findUnique({
+      where: { id },
+      include: { author: true },
+    });
+
+    if (!article) {
+      throw new NotFoundException(`Article #${id} not found`);
+    }
+
+    return article;
   }
 
   update(id: number, updateArticleDto: UpdateArticleDto) {
